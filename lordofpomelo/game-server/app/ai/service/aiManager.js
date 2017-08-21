b491c1bf177f9439b5d493485be1a4ceaa5cc3d5
zydesign <zydesign@126.com>
2017-08-21 14:23:20 +0800

    Update aiManager.js

--- lordofpomelo/game-server/app/ai/service/aiManager.js
+++ lordofpomelo/game-server/app/ai/service/aiManager.js
@@ -26,10 +26,11 @@ pro.stop = function() {
  * Add a character into ai manager.
  * Add a brain to the character if the type is mob.
  * Start the tick if it has not started yet.
+ * 增加一个角色到ai管理器。如果角色类型为怪物，增加一个大脑。如果没开始则开始计时。
  */
 pro.addCharacters = function(cs) {
 	 // return;
-	
+	//如果还没开始或已经结束，则不增加角色大脑
 	if(!this.started || this.closed) {
 		return;
 	}
@@ -39,11 +40,13 @@ pro.addCharacters = function(cs) {
 	}
 
 	//create brain for the character.
+	//给角色创建大脑，玩家或者怪物
 	//TODO: add a brain pool?
 	var c;
 	for(var i=0, l=cs.length; i<l; i++) {
 		c = cs[i];
 		var brain;
+		//如果c.type的类型为玩家
 		if(c.type ===EntityType.PLAYER) {
 			// continue;
 			if(this.players[c.entityId]) {
@@ -57,6 +60,7 @@ pro.addCharacters = function(cs) {
 			}));
 			this.players[c.entityId] = brain;
 		} else {
+			//如果c.type的类型不为玩家，并在还没有在怪物组里
 			if(this.mobs[c.entityId]) {
 				continue;
 			}
@@ -73,8 +77,10 @@ pro.addCharacters = function(cs) {
 
 /**
  * remove a character by id from ai manager
+ * 从ai管理器中通过id删除一个角色，（id为entityId）
  */
 pro.removeCharacter = function(id) {
+	//如果还没开始或已经结束，则不执行删除操作
 	if(!this.started || this.closed) {
 		return;
 	}
@@ -86,6 +92,7 @@ pro.removeCharacter = function(id) {
 /**
  * Update all the managed characters.
  * Stop the tick if there is no ai mobs.
+ * 更新角色和怪物大脑，如果没有怪物ai，停止时间
  */
 pro.update = function() {
 	//return;
