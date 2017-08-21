1afdedc032d19fbb0a9441caf17511f1e5e39354
zydesign <zydesign@126.com>
2017-08-21 19:06:53 +0800

    Update player.js

--- lordofpomelo/game-server/app/ai/brain/player.js
+++ lordofpomelo/game-server/app/ai/brain/player.js
@@ -1,30 +1,33 @@
-var TryAndAdjust = require('../node/tryAndAdjust');
-var TryAttack = require('../action/tryAttack');
-var TryPick = require('../action/tryPick');
-var TryTalkToNpc = require('../action/tryTalkToNpc');
-var MoveToTarget = require('../action/moveToTarget');
-var MoveToTargetForAttack = require('../action/moveToTargetForAttack');
+var TryAndAdjust = require('../node/tryAndAdjust');     //尝试调整
+var TryAttack = require('../action/tryAttack');         //尝试攻击
+var TryPick = require('../action/tryPick');             //尝试拾取
+var TryTalkToNpc = require('../action/tryTalkToNpc');   //尝试与NPC对话
+var MoveToTarget = require('../action/moveToTarget');   //移动到目标位置
+var MoveToTargetForAttack = require('../action/moveToTargetForAttack');   //移动到目标位置并进行攻击
 
-var bt = require('pomelo-bt');
-var Loop = bt.Loop;
-var If = bt.If;
-var Select = bt.Select;
+var bt = require('pomelo-bt');    //AI所依赖的行为树
+var Loop = bt.Loop;   //行为树的循环节点
+var If = bt.If;       //行为树的条件函数
+var Select = bt.Select;  //行为树的选择函数
 var consts = require('../../consts/consts');
 
 /**
  * Auto fight brain.
  * Attack the target if have any.
  * Choose the 1st skill in fight skill list or normal attack by defaul.
+ * 自动战斗大脑，如果有目标则发起攻击，选择第一个技能攻击或默认技能攻击
  */
 var Brain = function(blackboard) {
-	var attack = genAttackAction(blackboard);
-	var pick = genPickAction(blackboard);
-	var talkToNpc = genNpcAction(blackboard);
+	var attack = genAttackAction(blackboard);  //生成攻击行为
+	var pick = genPickAction(blackboard);      //生成拾取行为
+	var talkToNpc = genNpcAction(blackboard);  //生成NPC行为
 
+	//实例一个选择行为
 	var action = new Select({
 		blackboard: blackboard
 	});
 
+	//添加选择行为的子行为
 	action.addChild(attack);
 	action.addChild(pick);
 	action.addChild(talkToNpc);
@@ -35,10 +38,12 @@ var Brain = function(blackboard) {
 
 var pro = Brain.prototype;
 
+//更新行为
 pro.update = function() {
 	return this.action.doAction();
 };
 
+//生成攻击行为
 var genAttackAction = function(blackboard) {
 	//try attack and move to target action
 	var attack = new TryAndAdjust({
