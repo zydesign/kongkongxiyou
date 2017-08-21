eb935ae4a25c20be53ca838e9b1db922ebb3a60a
zydesign <zydesign@126.com>
2017-08-21 11:05:11 +0800

    Update ai.js

--- lordofpomelo/game-server/app/ai/ai.js
+++ lordofpomelo/game-server/app/ai/ai.js
@@ -5,9 +5,12 @@ var path = require('path');
 
 var exp = module.exports;
 
+//创建一个ai管理实例
 exp.createManager = function(opts) {
 	var brainService = new BrainService();
 	fs.readdirSync(__dirname + '/brain').forEach(function(filename){
+		//解析：/.../x,表示x文件夹里，查找某文件，反斜杠\表示查找内容
+		//这里是在test文件夹里面查找带js脚本的文件
 		if (!/\.js$/.test(filename)) {
 			return;
 		}
@@ -17,6 +20,7 @@ exp.createManager = function(opts) {
 	});
 
 	opts = opts || {};
+	//引入智慧服务
 	opts.brainService = brainService;
 	return new AiManager(opts);
 };
