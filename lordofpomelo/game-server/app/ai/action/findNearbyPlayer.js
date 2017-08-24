c5fcc300de2b8afba1a0c4c0e484a179d4d1ddea
zydesign <zydesign@126.com>
2017-08-24 18:37:49 +0800

    Update findNearbyPlayer.js

--- lordofpomelo/game-server/app/ai/action/findNearbyPlayer.js
+++ lordofpomelo/game-server/app/ai/action/findNearbyPlayer.js
@@ -10,7 +10,8 @@ var pro = Action.prototype;
 
 /**
  * Find the near by player and hates the player.
- *
+ * 找到附近玩家并锁定目标
+ * 
  * @return {Number} bt.RES_SUCCESS if find a player and hates him;
  *					bt.RES_FAIL if no player nearby.
  */
@@ -18,14 +19,17 @@ pro.doAction = function() {
 	var character = this.blackboard.curCharacter;
 	if(character.target || character.haters.length) {
 		//have a target already
+		//已经有锁定目标
 		return bt.RES_SUCCESS;
 	}
  
 	var area = this.blackboard.area;
 	//TODO: remove magic range: 300
+	//自身300范围内搜索玩家
 	var players = area.getEntitiesByPos({x: character.x, y: character.y}, [EntityType.PLAYER], 300); 
 	if(players && players.length) {
 		//TODO: remove magic hate point: 5
+		// 增加5点仇恨
 		character.increaseHateFor(players[0].enitityId, 5);
 		return bt.RES_SUCCESS;
 	}
