076bf5c8717fb8576e0fc37b1d3008de8ba0e421
zydesign <zydesign@126.com>
2017-08-28 16:55:35 +0800

    Update tryAttack.js

--- lordofpomelo/game-server/app/ai/action/tryAttack.js
+++ lordofpomelo/game-server/app/ai/action/tryAttack.js
@@ -27,22 +27,26 @@ var pro = Action.prototype;
  * bt.RES_SUCCESS if success to invoke the skill;
  * bt.RES_FAIL if any fails and set distanceLimit to blackboard stands for beyond the skill distance.
  */
+//判断当前技能.curSkill是否存在；判断当前目标curTarget是否存在；判断当前目标curTarget是否匹配角色锁定的目标target
 pro.doAction = function() {
 	var blackboard=this.blackboard;
 	var character = blackboard.curCharacter;
 
 	// console.log("AI tryAttack=============>>"+character.characterData.name);
 
+	//如果当期角色获取不到当前技能，返回失败
 	if(!character.curSkill){
 		// console.log("AI tryAttack=============>> !curSkill");
 		return bt.RES_FAIL;
 	}
 
-	//当前锁定的目标id
+	// 当前锁定的目标id，是从黑板中获取
+	// ps：blackboard.curTarget是由大脑player脚本的haveTarget赋值的，其if节点先条件赋值，再执行攻击节点
 	var targetId = blackboard.curTarget;
 	//通过目标id，使用场景获取目标实体
 	var target = blackboard.area.getEntity(targetId);
 
+	//如果获取不到target，或者target死亡
 	if(!target || target.died) {
 		// console.log("AI tryAttack=============>> !target");
 		// target has disappeared or died
@@ -54,6 +58,7 @@ pro.doAction = function() {
 		return bt.RES_FAIL;
 	}
 
+	//判断当前目标是否匹配角色锁定的目标
 	if(targetId !== character.target) {
 		// console.log("AI tryAttack=============>>target change");
 		//if target change abort current attack and try next action
@@ -66,8 +71,11 @@ pro.doAction = function() {
 	// 	target.type !== EntityType.PLAYER){
 	// 	return bt.RES_FAIL;
 	// }
+	
+	//这里执行攻击动作函数
 	var result = character.attack(target,character.curSkill);
-	//如果打出了攻击或打出了miss，就算攻击成功
+	
+	//如果打出了攻击或打出了miss，就算攻击成功，否则还是失败
 	if(result === AttackResult.SUCCESS
 		|| result === AttackResult.MISS 
 		) {
