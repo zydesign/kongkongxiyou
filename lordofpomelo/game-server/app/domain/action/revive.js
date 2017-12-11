7edc2026117eab98205e7501ac1827fe886ecf66
zydesign <zydesign@126.com>
2017-12-11 18:51:14 +0800

    Update revive.js

--- lordofpomelo/game-server/app/domain/action/revive.js
+++ lordofpomelo/game-server/app/domain/action/revive.js
@@ -12,6 +12,7 @@ var Revive = function(opts) {
 	Action.call(this, opts);
 	this.entityId=opts.entityId;
 	this.area = opts.area;
+	//reviveTime为复活时间间隔，deadline下一次能复活的时间点
 	this.deadline = opts.reviveTime + Date.now();
 };
 
@@ -22,6 +23,7 @@ module.exports = Revive;
  * @api public
  */
 Revive.prototype.update = function(currentTime) {
+	//当前时间达到了复活时间点，满足复活
 	if (currentTime >= this.deadline) {
 		var player=this.area.getEntity(this.entityId);
 		if (player) {
